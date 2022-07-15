50d3337d71976dcca0f838ddd3087937cb6ebb08
chen po hung <tdd057@gmail.com>
2022-07-16 00:48:47 +0800

    移除play變數

--- Refactoring.FirstExampleDemoTests/Theater.cs
+++ Refactoring.FirstExampleDemoTests/Theater.cs
@@ -6,8 +6,10 @@ namespace Refactoring.FirstExampleDemoTests
 {
     public class Theater
     {
+        private Dictionary<string, Play> _plays;
         public string Statement(Invoice invoice, Dictionary<string, Play> plays)
         {
+            _plays = plays;
             var totalAmount = 0;
             var volumeCredits = 0;
             var result = $"Statement for {invoice.Customer}\r\n";
@@ -19,7 +21,7 @@ namespace Refactoring.FirstExampleDemoTests
 
             foreach (var perf in invoice.Performances)
             {
-                var play = plays[perf.PlayId];
+                var play = PlayFor(perf);
                 var thisAmount = AmountFor(perf, play);
 
                 // add volume credits
@@ -34,34 +36,39 @@ namespace Refactoring.FirstExampleDemoTests
             result += $"Amount owed is {(totalAmount / 100).ToString("C", new CultureInfo("en-US"))}\r\n";
             result += $"You earned {volumeCredits} credits";
             return result;
-        }
 
-        private static int AmountFor(Performance aPerformance, Play play)
-        {
-            int result;
-            switch (play.Type)
+            Play PlayFor(Performance aPerformance)
             {
-                case "tragedy":
-                    result = 40000;
-                    if (aPerformance.Audience > 30)
-                    {
-                        result += 1000 * (aPerformance.Audience - 30);
-                    }
-                    break;
-                case "comedy":
-                    result = 30000;
-                    if (aPerformance.Audience > 20)
-                    {
-                        result += 10000 + 500 * (aPerformance.Audience - 20);
-                    }
-
-                    result += 300 * aPerformance.Audience;
-                    break;
-                default:
-                    throw new Exception($"unknown type: {play.Type}");
+                return _plays[aPerformance.PlayId];
             }
 
-            return result;
+            int AmountFor(Performance aPerformance, Play play)
+            {
+                int result;
+                switch (play.Type)
+                {
+                    case "tragedy":
+                        result = 40000;
+                        if (aPerformance.Audience > 30)
+                        {
+                            result += 1000 * (aPerformance.Audience - 30);
+                        }
+                        break;
+                    case "comedy":
+                        result = 30000;
+                        if (aPerformance.Audience > 20)
+                        {
+                            result += 10000 + 500 * (aPerformance.Audience - 20);
+                        }
+
+                        result += 300 * aPerformance.Audience;
+                        break;
+                    default:
+                        throw new Exception($"unknown type: {play.Type}");
+                }
+
+                return result;
+            }
         }
     }
 }
